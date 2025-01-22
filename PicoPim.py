35294174f6a5d4287f7e555e89d57de2b7bd61eb
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-22 11:21:16 +0100

    Update PicoPim.py

--- PicoPim.py
+++ PicoPim.py
@@ -3,13 +3,20 @@ import uasyncio
 from scd4x4 import SCD4X
 from ST7735 import TFT
 from seriffont import seriffont
+from trackball import Trackball
 
 i2c = I2C(0) 
 scd41 = SCD4X(i2c)
 scd41.start_periodic_measurement()
+#scd30.measurement_interval(5)
 co2 = 400
 temp = 25
 hum = 50
+
+trackball = Trackball( i2c )
+#trackball.set_rgbw(0, 0, 0, 0)
+Luz = "white"
+up, down, left, right, switch, state = trackball.read()
 
 spi = SPI(0, baudrate=20000000, polarity=0, phase=0, sck=Pin(18), mosi=Pin(19),miso=Pin(16))
 tft=TFT(spi,20,22,17)
@@ -17,6 +24,7 @@ tft.initr()
 tft.rgb(True)
 tft.rotation(1)
 lcd = "on"
+
 
 async def mesure():
     if scd41.data_ready:
@@ -48,10 +56,78 @@ async def affichage(co2,temp,hum,state):
             tft.text((10, 82), str(temp) + "C", TFT.WHITE, seriffont, 2, nowrap=True)
             tft.text((90, 82), str(hum) + "%", TFT.WHITE, seriffont, 2, nowrap=True)
 
+async def trackball_check(period_ms):
+    global up,down,left,right,state
+    down, up, right, left, switch, state = trackball.read()
+    #up, down, left, right, switch, state = trackball.read()
+    #print("r: {:02d} u: {:02d} d: {:02d} l: {:02d} switch: {:03d} state: {}".format(right, up, down, left, switch, state))
+
 async def main():
     while 1:
+        uasyncio.create_task(trackball_check(200))
         uasyncio.create_task(mesure())
         uasyncio.create_task(affichage(co2,temp,hum,state))
-        await uasyncio.sleep_ms(5100) 
+        uasyncio.create_task(luz(co2,up,down,left,right))
+        await uasyncio.sleep_ms(5100)
+        
+async def luz(co2,up,down,left,right):
+    global Luz
+    if up >= 10:
+        Luz = "on"
+    if down >= 10:
+        Luz = "off"
+        trackball.set_rgbw(0,0,0, 0)
+    if left >= 10:
+        Luz = "white"
+        trackball.set_rgbw(0,0,200, 255)
+    if right >= 10:
+        Luz = "green"
+        trackball.set_rgbw(0,255,0, 0)    
+    if Luz == "on":
+        if co2 < 400:
+            trackball.set_rgbw(255,255,255, 255)
+
+        if co2 >= 400 and co2 < 800:
+            a = 255 - round((co2 - 400) / 1.6)
+            b = 0
+            c = 255
+            trackball.set_rgbw(a,b,c, 0)
+    
+        if co2 >= 800 and co2 < 1200:
+            a = 0
+            b = round((co2 - 800) / 1.6)
+            c = 255
+            trackball.set_rgbw(a,b,c, 0)
+
+        if co2 >= 1200 and co2 < 1600:
+            a = 0
+            b = 255
+            c = 255 - round((co2 - 1200) / 1.6)
+            trackball.set_rgbw(a,b,c, 0)
+
+        if co2 >= 1600 and co2 < 2000:
+            a = round((co2 - 1600) / 1.6)
+            b = 255
+            c = 0
+            trackball.set_rgbw(a,b,c, 0)
+
+        if co2 >= 2000 and co2 < 2400:
+            a = 255
+            b = 255 - round((co2 - 2000) / 1.6)
+            c = 0
+            trackball.set_rgbw(a,b,c, 0)
+
+        if co2 >= 2400:
+            a = 255
+            b = 0
+            c = 0
+            trackball.set_rgbw(a,b,c, 0)
+    if Luz == "off":
+        trackball.set_rgbw(0,0,0, 0)
+    if Luz == "white":
+        trackball.set_rgbw(0,0,200, 255)
+    if Luz == "green":
+        trackball.set_rgbw(0,255,0, 0)
+            
 uasyncio.run(main())
 
